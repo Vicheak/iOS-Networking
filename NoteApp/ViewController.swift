07965515fc4e05e0a0bc67e8dc732de9b97cdcde
Vicheak <suonvicheak991@gmail.com>
2024-06-26 22:47:11 +0700

    Initial Commit

---
+++ NoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteApp
+//
+//  Created by @suonvicheakdev on 26/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
